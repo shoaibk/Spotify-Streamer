db9d39a533f34a2b56ada1fe1f12682cfbc95699
Shoaib Khan <shb.khan@gmail.com>
2015-06-21 16:17:30 -0400

    Progressive search implemented with TextWatcher interface. Issues: Toast spamming. Instead of toast, need to implement a layout to show when no artists found or the search field is empty

--- app/src/main/java/ca/shoaib/spotifystreamer/SearchArtistActivity.java
+++ app/src/main/java/ca/shoaib/spotifystreamer/SearchArtistActivity.java
@@ -1,21 +1,18 @@
 package ca.shoaib.spotifystreamer;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -36,7 +33,7 @@ public class SearchArtistActivity extends AppCompatActivity {
     /**
      * TODO: use progressive search
      * TODO:
-
+     * <p/>
      * TODO: create Playback activity
      */
 
@@ -58,49 +55,51 @@ public class SearchArtistActivity extends AppCompatActivity {
         adapter = new ArtistAdapter(this,
                 android.R.layout.simple_list_item_1, artistsInListView);
 
-        ListView artistList = (ListView)findViewById(R.id.list_artist);
+        ListView artistList = (ListView) findViewById(R.id.list_artist);
         artistList.setAdapter(adapter);
 
         searchInput = (EditText) findViewById(R.id.search_artist);
 
-        if( savedInstanceState != null ) {
+        searchInput.addTextChangedListener(textChangeObserver);
+
+        if (savedInstanceState != null) {
             artistsInListView = savedInstanceState.getParcelableArrayList(KEY_ARTISTS);
             adapter = new ArtistAdapter(this,
                     android.R.layout.simple_list_item_1, artistsInListView);
             artistList.setAdapter(adapter);
         }
 
-        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
-                if (actionId == EditorInfo.IME_ACTION_SEND) {
-                    String searchText = searchInput.getText().toString();
-
-                    if(!searchText.isEmpty()) {
-                        if(Utilities.isOnline(getApplicationContext())) {
-                            new SearchArtistTask().execute(searchText);
-                        } else {
-                            Utilities.showToastOffline(getApplicationContext());
-                        }
-
-                        // hide keyboard
-                        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                        in.hideSoftInputFromWindow(v.getWindowToken(),
-                                InputMethodManager.HIDE_NOT_ALWAYS);
-
-                        handled = true;
-                    }
-                }
-                return handled;
-            }
-        });
+//        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                boolean handled = false;
+//                if (actionId == EditorInfo.IME_ACTION_SEND) {
+//                    String searchText = searchInput.getText().toString();
+//
+//                    if(!searchText.isEmpty()) {
+//                        if(Utilities.isOnline(getApplicationContext())) {
+//                            new SearchArtistTask().execute(searchText);
+//                        } else {
+//                            Utilities.showToastOffline(getApplicationContext());
+//                        }
+//
+//                        // hide keyboard
+//                        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//                        in.hideSoftInputFromWindow(v.getWindowToken(),
+//                                InputMethodManager.HIDE_NOT_ALWAYS);
+//
+//                        handled = true;
+//                    }
+//                }
+//                return handled;
+//            }
+//        });
 
 
         artistList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(Utilities.isOnline(getApplicationContext())) {
+                if (Utilities.isOnline(getApplicationContext())) {
                     Intent intent = new Intent(getApplicationContext(), TopTracksActivity.class);
                     ArtistData chosenArtist = artistsInListView.get(position);
                     intent.putExtra(ARTIST_ID, chosenArtist.getArtistId());
@@ -111,6 +110,31 @@ public class SearchArtistActivity extends AppCompatActivity {
             }
         });
     }
+
+    TextWatcher textChangeObserver = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            new SearchArtistTask().execute(s.toString());
+
+            if (!s.toString().isEmpty()) {
+                if (Utilities.isOnline(getApplicationContext())) {
+                    new SearchArtistTask().execute(s.toString());
+                } else {
+                    Utilities.showToastOffline(getApplicationContext());
+                }
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -152,7 +176,7 @@ public class SearchArtistActivity extends AppCompatActivity {
                 SpotifyService spotify = api.getService();
                 ArtistsPager results = spotify.searchArtists(artistName[0]);
 
-                for(Artist artist: results.artists.items) {
+                for (Artist artist : results.artists.items) {
                     ArtistData artistData = new ArtistData(artist);
                     artistListData.add(artistData);
                     Log.d(TAG, artistData.toString());
@@ -168,7 +192,7 @@ public class SearchArtistActivity extends AppCompatActivity {
         }
 
         protected void onPostExecute(List<ArtistData> artists) {
-            if(artists.isEmpty()){
+            if (artists.isEmpty()) {
                 Toast.makeText(getApplicationContext(), R.string.no_artist_toast, Toast.LENGTH_LONG).show();
 
             } else {
